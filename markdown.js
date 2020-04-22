53810a56910e27d492c4d82ab95aebeaa1504a6e
Tom Nielsen <tanielsen@gmail.com>
2020-04-22 14:53:44 +0100

    fieldviews

--- markdown.js
+++ markdown.js
@@ -4,18 +4,23 @@ const md = require("markdown-it")();
 module.exports = {
   name: "Markdown",
   sql_name: "text",
-  editAs: (nm, v, attrs, cls) =>
-    textarea(
-      {
-        class: `form-control ${cls || ""}`,
-        name: nm,
-        id: `input${nm}`,
-        rows: 10
-      },
-      v || ""
-    ),
-  showAs: v => md.render(v),
-  listAs: v=> v.length >10 ? v.substring(0,10): v,
+  fieldviews: {
+    showAll: { isEdit: false, run: v => md.render(v) },
+    peek: { isEdit: false, run: v => (v.length > 10 ? v.substring(0, 10) : v) },
+    edit: {
+      isEdit: true,
+      run: (nm, v, attrs, cls) =>
+        textarea(
+          {
+            class: `form-control ${cls || ""}`,
+            name: nm,
+            id: `input${nm}`,
+            rows: 10
+          },
+          v || ""
+        )
+    }
+  },
   read: v => {
     switch (typeof v) {
       case "string":
